24625c272be420974d917cf19e7e497436319e7c
redsquirrelstudio <lewisraggett17@gmail.com>
2021-04-10 19:49:28 +0100

    added target blank to button

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -87,7 +87,7 @@
                                         <td>
                                             <div class="row no-gutters">
                                                 <div class="col-auto">
-                                                    <a class="btn btn-sm btn-spinner btn-primary" :href="item.page_url" title="Go To Page" role="button"><i class="fa fa-external-link"></i></a>
+                                                    <a class="btn btn-sm btn-spinner btn-primary" target="_blank" :href="item.page_url" title="Go To Page" role="button"><i class="fa fa-external-link"></i></a>
                                                 </div>
                                                 <div class="col-auto">
                                                     <a class="btn btn-sm btn-spinner btn-info" :href="item.resource_url + '/edit'" title="{{ trans('brackets/admin-ui::admin.btn.edit') }}" role="button"><i class="fa fa-edit"></i></a>
