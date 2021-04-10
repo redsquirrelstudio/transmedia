63b35aacc0c7c4aa5d9c10f82f3962dc38b29cc7
redsquirrelstudio <lewisraggett17@gmail.com>
2021-04-10 19:48:38 +0100

    fixed icon

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -87,7 +87,7 @@
                                         <td>
                                             <div class="row no-gutters">
                                                 <div class="col-auto">
-                                                    <a class="btn btn-sm btn-spinner btn-info" :href="item.page_url" title="Go To Page" role="button"><i class="fa fa-external-link-alt"></i></a>
+                                                    <a class="btn btn-sm btn-spinner btn-info" :href="item.page_url" title="Go To Page" role="button"><i class="fa fa-external-link"></i></a>
                                                 </div>
                                                 <div class="col-auto">
                                                     <a class="btn btn-sm btn-spinner btn-info" :href="item.resource_url + '/edit'" title="{{ trans('brackets/admin-ui::admin.btn.edit') }}" role="button"><i class="fa fa-edit"></i></a>
