362510a7413cfada238a5e6f6ce18a8c6aae8a59
redsquirrelstudio <lewisraggett17@gmail.com>
2021-04-14 09:27:35 +0100

    updated about

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ if (env('SHOW_LANDING')) {
     });
 } else {
     Route::get('/', [PageController::class, 'index'])->name('page.index');
-    Route::get('/about', [PageController::class, 'about'])->name('page.about');
-}
 
+}
+Route::get('/about', [PageController::class, 'about'])->name('page.about');
 Route::get('/exhibition', [PageController::class, 'exhibition'])->name('page.exhibition');
 
 Route::group(['middleware' => 'auth'], function(){
