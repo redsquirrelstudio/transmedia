37bfab6c06e526adac8bf1acc8248dc91129be76
Lewis Raggett <46108903+redsquirrelstudio@users.noreply.github.com>
2022-03-17 17:31:26 +0000

    Hotfix

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,11 +13,11 @@
           rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/icons//apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/icons//favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/icons//favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('/icons//site.webmanifest') }}">
-    <link rel="mask-icon" href="{{ asset('/icons//safari-pinned-tab.svg') }}" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/icons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/icons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/icons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('/icons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('/icons/safari-pinned-tab.svg') }}" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#9f00a7">
     <meta name="theme-color" content="#ffffff">
 </head>
