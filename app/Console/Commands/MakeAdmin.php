55cfca93f2df08898906243ba2c6e7d08f0e83d0
sineadmarais <91264541+sineadmarais@users.noreply.github.com>
2022-04-05 21:10:44 +0100

    added admin command

---
+++ app/Console/Commands/MakeAdmin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class MakeAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:admin {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        DB::table('admin_users')->insert([
+            [
+                'email'=>'sinead.marais98@gmail.com',
+                 'password'=>Hash::make($this->argument('password')),
+                 'first_name'=>'sinead',
+                 'last_name'=>'marais',
+                 'activated'=>true,
+                 'forbidden'=>false,
+                 'language'=>'en',
+
+            ]
+            ]);
+            $this->info('user created');
+        return 0;
+    }
+}
