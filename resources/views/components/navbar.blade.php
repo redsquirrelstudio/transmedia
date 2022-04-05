5b7a3075016954505e63adb0bef2bc4b730a75bc
Lewis Raggett <lewisraggett17@gmail.com>
2022-04-05 22:11:08 +0100

    2022 Reset, added base layout, navigation and SCSS structure

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,65 @@
+<div  x-data="{ open: false }">
+    <nav class="navbar">
+        <div class="container-fluid">
+            <ul>
+                <li>
+                    <button x-on:click="open = !open" x-bind:class="open ? 'is-active':''"
+                            class="hamburger hamburger--collapse" type="button">
+                      <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                      </span>
+                    </button>
+                </li>
+                <li class="nav-brand">
+                    <a href="{{ url('/') }}">
+                        <h1>
+                            <span>Trans</span>media {{ date('Y') }}
+                        </h1>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div x-show="open" x-cloak
+         x-transition:enter="entering"
+         x-transition:enter-start="enter-start"
+         x-transition:enter-end="enter-end"
+         x-transition:enter="leaving"
+         x-transition:leave-start="leave-start"
+         x-transition:leave-end="leave-end"
+         class="nav-menu">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <a class="link-1" href="">
+                        Virtual Exhibition
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <a class="link-2" href="">
+                        Pitch your project
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <a class="link-3" href="">
+                        Students
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <a class="link-4" href="">
+                        About
+                    </a>
+                    <a class="link-5" href="">
+                        {{ date('Y') }} Projects
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Transmedia {{ date('Y') }} - This Year We're Breaking Out</title>
+    <meta name="description"
+          content="The official website for {{ date('Y') }}'s Transmedia exhibition hosted by the students of the University of Winchester's digital courses.">
+
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body>
+<div class="site-container">
+    @include('components/navbar')
+    <div>
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
