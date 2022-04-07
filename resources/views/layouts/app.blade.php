6daa8416faa8721080b0a4f6cdd691d8eeeea8fa
Lewis Raggett <lewisraggett17@gmail.com>
2022-04-07 21:30:32 +0100

    Hotfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,16 +12,15 @@
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet">
 
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 <body>
 <div class="site-container">
     @include('components/navbar')
-    <div>
-        @yield('content')
-    </div>
+    @yield('content')
 </div>
 </body>
 </html>
