4e3c33c3a82a0df31921946b29771c8d60f88340
sineadmarais <91264541+sineadmarais@users.noreply.github.com>
2022-04-07 23:40:40 +0100

    about updates

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<!doctype html>
-<html lang="en">
-<body>
-<h1>  about page  </h1>
-</body>
-</html>
+<div class="about_container">
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 headertext">
+            <h2><span class="orangeheadtext">Our Mission.</span> To create a phyiscal and virtual exhibition to showcase our students work</h2>
+            <div class="spacer"></div>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<div class="padding30"></div>
+            <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+        <div class="col-md-5">
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto roundedimg" width="500"
+                height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
+                preserveAspectRatio="xMidYMid slice" focusable="false">
+
+                <img class="aboutheadimg" src="..public/images/library.jpg" alt="aboutheadimg">
+
+                <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
+                    dy=".3em">500x500</text>
+            </svg>
+
+        </div>
+    </div>
+</div>
+</div>
+
 
 
 @endsection
