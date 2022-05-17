d65685e39f4d8c7f8b1d08e75685a2cba9dc6f6c
Lewis Raggett <46108903+redsquirrelstudio@users.noreply.github.com>
2022-05-17 17:45:16 +0100

    Hotfix

--- resources/views/exhibition.blade.php
+++ resources/views/exhibition.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="screen-block">
        <div class="twinmotion-embed-wrapper">
-            <iframe style="height: 100%; width: 100%; min-height: 375px; min-width: 375px;"
+            <iframe style="height: 100%; width: 100%; min-height: 100vh; min-width: 100vw;"
                     title="Embedded presentation 'Transmedia Exhibition 2022'" frameborder="0"
                     allow="fullscreen; gyroscope; accelerometer; magnetometer; execution-while-out-of-viewport; execution-while-not-rendered;"
                     allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"
